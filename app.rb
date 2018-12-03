5a40e896571c22ff75fba543144d3988db7d9c36
Dana Gajewska <mdgajewska@outlook.com>
2018-12-03 15:00:15 +0000

    Adds ability to view individual spaces

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require 'sinatra/base'
 class MakersBNB < Sinatra::Base
 
   get '/' do
-    @spaces = space.all? #LOOK AT METHOD NAME 
+    @spaces = Space.all? #LOOK AT METHOD NAME
     erb(:index)
+  end
+
+  get '/spaces/:id' do
+    @space = Space.find(id: params[:id]) #DOUBLE CHECK METHOD
+    erb :'spaces/view'
   end
 
 run! if app_file == $0
--- spec/features/viewing_spaces_spec.rb
+++ spec/features/viewing_spaces_spec.rb
@@ -2,20 +2,19 @@ feature 'Viewing available spaces' do
   scenario 'A user can view spaces' do
     visit('/')
     expect(page).to have_content "Makers BNB"
+    expect(page).to have_content "'Spacious 2 bedroom near city centre'"
   end
 
-  # scenario 'Adding a peep' do
-  #   visit('/chitter')
-  #   click_button('Add user')
-  #   fill_in('username', with: 'testuser')
-  #   fill_in('password', with: 'testpassword')
-  #   click_button('Submit')
-  #   click_button('Sign in')
-  #   fill_in('username', with: 'testuser')
-  #   fill_in('password', with: 'testpassword')
-  #   click_button('Sign in')
-  #   fill_in('content', with: 'This is my second peep')
-  #   click_button('Add peep')
-  #   expect(page).to have_content "This is my second peep"
-  # end
+  scenario 'View individual space page' do
+    visit('/')
+    click_button ('view-space-1')
+    expect(page).to have_content 'Spacious 2 bedroom near city centre'
+    expect(page).to have_content '£130 per night'
+    expect(page).to have_content '4 guests'
+    expect(page).to have_content '2 bedrooms'
+    expect(page).to have_content '2 beds'
+    expect(page).to have_content '1 bathroom'
+    expect(page).to have_content 'Dana'
+    expect(page).to have_content '1234567890'
+  end
 end
