5371c0a383480291a512096c946b3294e59be652
Max Stevenson <maxstevenson@msn.com>
2018-12-04 10:43:21 +0000

    - Fixed conflict code additions.

--- app.rb
+++ app.rb
@@ -3,27 +3,12 @@ require 'sinatra/base'
 require 'sinatra/flash'
 require_relative './dm'
 
-<<<<<<< HEAD
-if ENV['ENVIRONMENT'] == 'test'
-	set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'makersbnb_test', pool: 2}	
-else
-	set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'makersbnb', pool: 2}
-end
-
-class MakersBNB < Sinatra::Base
-
-	get '/' do
-    @spaces = Space.all? #LOOK AT METHOD NAME
-=======
-
-
 class MakersBNB < Sinatra::Base
 	enable :sessions
 	register Sinatra::Flash
 
 	get '/' do
     @properties = Property.all #LOOK AT METHOD NAME
->>>>>>> 1eca0c48e5541b8abedac102e62d0ea35846b9c0
     erb(:index)
   end
 
