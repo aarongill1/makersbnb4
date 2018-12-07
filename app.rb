79d5bc333fb19c55b833e3464aa1d4167e7e2138
Max Stevenson <maxstevenson@msn.com>
2018-12-07 11:37:25 +0000

    - Test written for log out feature. - Log out functionality added to profile page only. - Controller route resets session id to nil upon log out.

--- app.rb
+++ app.rb
@@ -70,6 +70,11 @@ class MakersBNB < Sinatra::Base
 		end
 	end
 
+	post '/user/logout' do
+		session[:id] == nil
+		redirect '/'
+	end
+
   post '/property/create' do
 		@property = Property.create(
       user_id: session[:id],
---
+++ credentials.rb
@@ -0,0 +1,5 @@
+class Credentials
+	attr_reader :origin_email, :acc_password
+	@origin_email = 'makerscourseemailtesting@gmail.com'
+	@acc_password = '$Makers20018'
+end
